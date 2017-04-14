ff563f33a803dfff174e8409e7e948882bec3611
Scott Warchal <S.Warchal@sms.ed.ac.uk>
2017-04-14 17:21:51 +0100

    solution to problem 21 in c

---
+++ problem_21.c
@@ -0,0 +1,49 @@
+/* project euler # 21
+ *
+ * Let d(n) be defined as the sum of proper divisors of n (numbers less than n
+ * which divide evenly into n).
+ * If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and
+ * each of a and b are called amicable numbers.
+ *
+ * For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55
+ * and 110; therefore d(220) = 284. The proper divisors of 284 are
+ * 1, 2, 4, 71 and 142; so d(284) = 220.
+ *
+ * Evaluate the sum of all the amicable numbers under 10000
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+// sum of proper divisors
+int d(int n)
+{
+    int total = 0;
+    for (int i=1; i<n; i++)
+    {
+        if (n % i == 0)
+        {
+            total += i;
+        }
+    }
+    return total;
+}
+
+
+int main()
+{
+    int total = 0;
+
+    for (int i=1; i<10000; i++)
+    {
+        int sum_div = d(i);
+
+        if (d(sum_div) == i && sum_div != i)
+        {
+            total += sum_div + i;
+        }
+    }
+    // duplicated each pair, so bodge is to just halve the total
+    printf("%i\n", total/2);
+    return 0;
+}
