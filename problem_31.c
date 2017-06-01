de97b539419199c81fccdaac5ee37ec759476b5e
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-06-01 18:16:31 +0100

    tidier

--- problem_31.c
+++ problem_31.c
@@ -2,18 +2,16 @@
 
 int main()
 {
-    int target = 200;
+    const int target = 200;
     int coins[] = {1, 2, 5, 10, 20, 50, 100, 200};
-    int n[target];
 
     // initialise array to all zeros, first value as 1
+    int n[target];
     for (int i = 0; i <= target; i++) n[i] = 0;
     n[0] = 1;
 
-    for (int j = 0; j <= 8; j++)
-    {
-        for (int k = coins[j]; k <= target; k++)
-        {
+    for (int j = 0; j <= 8; j++) {
+        for (int k = coins[j]; k <= target; k++) {
             n[k] += n[k - coins[j]];
         }
     }
