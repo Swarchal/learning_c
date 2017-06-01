58d8fb916dc4f01f52fa2285b171b56b0891b7f1
Scott Warchal <s.warchal@sms.ed.ac.uk>
2017-06-01 18:10:30 +0100

    solutions for problem 31

---
+++ problem_31.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int target = 200;
+    int coins[] = {1, 2, 5, 10, 20, 50, 100, 200};
+    int n[target];
+
+    // initialise array to all zeros
+    for (int i = 0; i <= target; i++)
+    {
+        n[i] = 0;
+    }
+
+    // set first value to 1
+    n[0] = 1;
+
+    for (int j = 0; j <= 8; j++)
+    {
+        for (int k = coins[j]; k <= target; k++)
+        {
+            n[k] += n[k - coins[j]];
+        }
+    }
+
+    printf("%i\n", n[target]);
+    return 0;
+}
---
+++ problem_31.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+project euler # 31
+how many different ways can £2 be made using any number of coins?
+"""
+
+target = 200
+coins = [1, 2, 5, 10, 20, 50, 100, 200]
+n = [1] + [0] * target
+
+for coin in coins:
+    for j in range(coin, target+1):
+        n[j] += n[j - coin]
+
+print(n[-1])
+
